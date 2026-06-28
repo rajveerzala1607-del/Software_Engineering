91535f59d576634440eb68291f449e6ad336f757
rajveerzala1607-del <rajveerzala1607@gmail.com>
2026-06-28 11:51:19 +0530

    c

--- c/07_array/01_array.c
+++ c/07_array/01_array.c
@@ -2,12 +2,13 @@
 #include<conio.h>
 
 void main()
+
 {
-//	index    =   0, 1, 2, 3, 4 
+	
 	int a[5] = {10,20,30,40,50};
 	
-	printf("%d",a[1]);
-	printf("\na : %d",a[3]);
-	
-	getch();
+	printf("%d", a[1]);
+	printf("\na : %d" ,a[3]);
+		
+	getch ();
 }
--- c/07_array/02_alldata.c
+++ c/07_array/02_alldata.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 #include<conio.h>
- 
- void main()
- 
- {
- 	
- 	int a[5] = {10,20,30,40,50};
+
+void main()
+{
+	int a[5] = {10,20,30,40,50};
 	int i;	
-		
-		for(i=0;i<5;i++){
-		printf("%d\n",i);
- }
- 	getch();
- 
- }
+
+
+	for(i=0;i<5;i++){
+//		printf("%d\n",i);
+//		printf("%d\n",a[i]);
+		printf("a[%d] : %d\n",i,a[i]);
+	}
+
+	
+	getch();
+}
