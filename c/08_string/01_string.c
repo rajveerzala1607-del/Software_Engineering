4e70dc8107506d9829fae550e91cd56bc87415a5
rajveerzala1607-del <rajveerzala1607@gmail.com>
2026-06-30 23:48:32 +0530

    c

---
+++ c/08_string/01_string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char name = 'r'; 
+	printf("%c",name);
+	
+	char demo[20] = "rajveer zala";
+	
+	printf("\nName : %s",demo);
+	
+	return 0;
+}
---
+++ c/08_string/02_string_use.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	char name[20];
+	
+	printf("Enter your Name :");
+
+	
+	gets(name);
+	
+	printf("Your Name : %s",name);
+	
+	getch();
+}
